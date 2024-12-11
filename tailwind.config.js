54ab136e45fcbe52194d1e754c597c85ce8e3083
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2024-12-11 14:24:26 -0300

    Ajuste de configuração

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Todos os arquivos do React
+  ],
   theme: {
-    extend: {},
+      extend: {
+        colors: {
+          background: 'hsl(var(--background))',
+          foreground: 'hsl(var(--foreground))',
+          card: 'hsl(var(--card))',
+          'card-foreground': 'hsl(var(--card-foreground))',
+          primary: 'hsl(var(--primary))',
+          'primary-foreground': 'hsl(var(--primary-foreground))',
+        },
+      }
   },
-  plugins: [],
-}
-
+  plugins: [],
+};
