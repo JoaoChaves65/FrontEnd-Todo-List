039777f3557f61111323d8dd1f6df1c57294dfa7
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2024-12-11 14:23:00 -0300

    Adicionando o conteúdo do App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import TodoList from "./components/TodoList"
+import { TodoProvider } from "./contexts/todoContext"
+import { Toaster } from "react-hot-toast"
+import { ThemeToggle } from "./components/ThemeToggle"
 
-function App() {
-
+export default function Home() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <div className="min-h-screen bg-background text-foreground">
+      <TodoProvider>
+        {/* Cabeçalho apenas com o botão de alternância */}
+        <header className="flex justify-end p-4 bg-card text-card-foreground">
+          <ThemeToggle />
+        </header>
+
+        {/* Lista de Tarefas */}
+        <main className="p-4">
+          <TodoList />
+        </main>
+      </TodoProvider>
+
+      {/* Notificações */}
+      <Toaster position="top-center" />
+    </div>
   )
 }
-
-export default App
